3b142f8f0c68edc0b83b04d5ca797cfcef9994fb
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2025-07-19 02:10:50 -0400

    Initial commit

---
+++ drupal_dump.sql
@@ -0,0 +1,8 @@
+
+Deprecated: Consolidation\Config\Config::__construct(): Implicitly marking parameter $data as nullable is deprecated, the explicit nullable type must be used instead in /Users/farahnoshyousofi/Qatra/vendor/consolidation/config/src/Config.php on line 33
+
+Deprecated: Robo\Common\IO::restoreState(): Implicitly marking parameter $input as nullable is deprecated, the explicit nullable type must be used instead in /Users/farahnoshyousofi/Qatra/vendor/consolidation/robo/src/Common/IO.php on line 48
+
+Deprecated: Robo\Common\IO::restoreState(): Implicitly marking parameter $output as nullable is deprecated, the explicit nullable type must be used instead in /Users/farahnoshyousofi/Qatra/vendor/consolidation/robo/src/Common/IO.php on line 48
+
+Deprecated: Robo\Common\IO::restoreState(): Implicitly marking parameter $io as nullable is deprecated, the explicit nullable type must be used instead in /Users/farahnoshyousofi/Qatra/vendor/consolidation/robo/src/Common/IO.php on line 48
